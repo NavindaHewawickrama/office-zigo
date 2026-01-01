66e5d6109aac62ca8224894b22318db526accae3
NavindaHewawickrama <hewawickraman@gmail.com>
2026-01-01 19:25:49 +0530

    new packages installed push 1/1/2026

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -179,12 +179,12 @@ const Navbar = () => {
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path
+                      {/* <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         strokeWidth={2}
                         d="M19 9l-7 7-7-7"
-                      />
+                      /> */}
                     </svg>
                   </button>
                 ) : (
@@ -200,12 +200,12 @@ const Navbar = () => {
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path
+                      {/* <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
                         strokeWidth={2}
                         d="M19 9l-7 7-7-7"
-                      />
+                      /> */}
                     </svg>
                   </Link>
                 )}
